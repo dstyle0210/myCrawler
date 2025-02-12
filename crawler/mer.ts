2378cf99fb00b86022c6361a12af12b2edba4609
dstyle0210 <dstyle0210@gmail.com>
2025-02-12 11:21:37 +0900

    수정

--- crawler/mer.ts
+++ crawler/mer.ts
@@ -1,40 +1,37 @@
-import {chromium} from "playwright";
-import TelegramBot from "node-telegram-bot-api";
-const {TG_TOKEN_MABONGPAPA,TG_CHATID_MABONGPAPA} = process.env; // ENV
+import dotenv from 'dotenv';
+import request from 'request';
+const {TG_TOKEN_DSTYLESTOCK,TG_CHATID_DSTYLESTOCK} = process.env; // ENV
+if (!TG_TOKEN_DSTYLESTOCK || !TG_CHATID_DSTYLESTOCK) {
+    dotenv.config();
+};
 
-(async () => {
-    // playwright browser open
-    const browser = await chromium.launch({headless:true});
-    const page = await browser.newPage();
-
-    const setMM = (mm:number) => ((mm<10) ? "0"+mm : ""+mm);
-    const convertDate = (date:string) => "20"+date.replace(/[^0-9]/g,"");
-    const nowDate = new Date();nowDate.setHours(nowDate.getHours() + 9); // github action UTC+0
-    const today = nowDate.getFullYear() + setMM(nowDate.getMonth()+1) + setMM(nowDate.getDate());
-    // const today = "20250203";
-
-
-    // 추가 목록
-    await page.goto("https://orangeboard.co.kr/portfolios/%40merXob?tabKey=3");
-    await new Promise((res) => setTimeout(res,5000));
-    const addList = await page.evaluate(() => {
-        return $("tr[class^='PortfolioTable__Primary'] td:last-child").map((idx,td)=> $(td).text() ).toArray();
-    });
-    const isAdd = addList.find((date) => (today == convertDate(date)));
-
-    // 삭제 목록
-    await page.goto("https://orangeboard.co.kr/portfolios/%40merXob?tabKey=4");
-    await new Promise((res) => setTimeout(res,5000));
-    const exitList = await page.evaluate(() => {
-        return $("tr[class^='PortfolioTable__Primary'] td:last-child").map((idx,td)=> $(td).text() ).toArray();
-    });
-    const isExit = exitList.find((date) => (today == convertDate(date)));
-
-    await browser.close();
-
-    // 텔레그램 발송
-    if(!TG_TOKEN_MABONGPAPA || !TG_CHATID_MABONGPAPA){ return; };
-    const bot = new TelegramBot(TG_TOKEN_MABONGPAPA, {polling: false});
-    const message = "[MER]\n추가:"+(!!isAdd)+"\n삭제:"+(!!isExit);
-    bot.sendMessage(TG_CHATID_MABONGPAPA, message);
-})();
+const setMM = (mm:number) => ((mm<10) ? "0"+mm : ""+mm);
+const nowDate = new Date();nowDate.setHours(nowDate.getHours() + 9); // github action UTC+0
+const today = nowDate.getFullYear() + setMM(nowDate.getMonth()+1) + setMM(nowDate.getDate());
+const convertDate = (date:string) => date.replace(/[^0-9]/g,"");
+// const today2 = "20250203";
+request.get({
+    url: "https://api.orangeboard.co.kr/v2/portfolio/@merXob/realtime"
+}, function (error, response, body) {
+    let text = "";
+    if (!error && response.statusCode == 200) {
+        const data = JSON.parse(body);
+        const result = data.stockTable.find((stock) => {
+            return today == convertDate(stock.startTrdDd);
+        });
+        // console.log(corpName , avgPrc);
+        if(result){
+            const {corpName , avgPrc} = result;
+            text = corpName+" "+avgPrc+"원";
+        }else{
+            text = "신규종목없음";
+        };
+    } else {
+        // console.log('error = ' + response.statusCode);
+        text = "에러" + response.statusCode;
+        
+    };
+    const post = `https://api.telegram.org/bot${TG_TOKEN_DSTYLESTOCK}/sendmessage?chat_id=${TG_CHATID_DSTYLESTOCK}&text=[메르] ${text}`;
+    // console.log(text);
+    request.get(post);
+});
