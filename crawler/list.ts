45c788cf4eb838aa7d204685f32ddf8ae43683d8
dstyle0210 <dstyle0210@gmail.com>
2025-02-14 14:56:00 +0900

    업데이트

---
+++ crawler/list.ts
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv';
+import request from 'request';
+import {initializeApp} from "firebase/app";
+import { getDatabase, set, get, ref, goOffline } from "firebase/database";
+
+dotenv.config();
+const {FIREBASE_URL,LIST_URL} = process.env; // ENV
+const firebaseConfig = {
+    databaseURL: FIREBASE_URL,
+};
+const getStockTable = () => {
+    return new Promise((res,rej)=>{
+        request.get({
+            url: LIST_URL
+        }, function (error, response, body) {
+            const data = JSON.parse(body);
+            res(data.stockTable);
+        });
+    });
+};
+
+(async () => {
+    const refData = await getStockTable();
+    const app = initializeApp(firebaseConfig);
+    const db = getDatabase(app);
+    const nowRef = ref(db, `nowList`);
+    const prevRef = ref(db, `prevList`);
+
+    await get(nowRef).then(async (snapshot) => {
+        if (snapshot.exists()) {
+            await set(prevRef,snapshot.val());
+            console.log(snapshot.val().length);
+        } else {
+            console.log("No data available");
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+
+    await set(nowRef,null);
+    await set(nowRef,refData);
+
+    goOffline(db);
+})();
--- crawler/mer.ts
+++ crawler/mer.ts
@@ -1,18 +1,16 @@
 import dotenv from 'dotenv';
 import request from 'request';
 import TelegramBot from "node-telegram-bot-api";
-const {TG_TOKEN_DSTYLESTOCK,TG_CHATID_DSTYLESTOCK} = process.env; // ENV
-if (!TG_TOKEN_DSTYLESTOCK || !TG_CHATID_DSTYLESTOCK) {
-    dotenv.config();
-};
+dotenv.config();
 
+const {TG_TOKEN_DSTYLESTOCK,TG_CHATID_DSTYLESTOCK,LIST_URL} = process.env; // ENV
 const setMM = (mm:number) => ((mm<10) ? "0"+mm : ""+mm);
 const nowDate = new Date();nowDate.setHours(nowDate.getHours() + 9); // github action UTC+0
 const today = nowDate.getFullYear() + setMM(nowDate.getMonth()+1) + setMM(nowDate.getDate());
 const convertDate = (date:string) => date.replace(/[^0-9]/g,"");
 // const today2 = "20250203";
 request.get({
-    url: "https://api.orangeboard.co.kr/v2/portfolio/@merXob/realtime"
+    url: LIST_URL
 }, function (error, response, body) {
     let text = "";
     if (!error && response.statusCode == 200) {
@@ -34,6 +32,6 @@ request.get({
 
     if(!TG_TOKEN_DSTYLESTOCK || !TG_CHATID_DSTYLESTOCK){ return; };
     const bot = new TelegramBot(TG_TOKEN_DSTYLESTOCK, {polling: false});
-    const message = "[메르] "+text;
+    const message = text;
     bot.sendMessage(TG_CHATID_DSTYLESTOCK, message);
 });
