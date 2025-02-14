171472a1e868dc94d6d9748230694ebe6b1f78ba
dstyle0210 <dstyle0210@gmail.com>
2025-02-14 16:49:37 +0900

    4444

--- crawler/list.ts
+++ crawler/list.ts
@@ -5,15 +5,24 @@ import { getDatabase, set, get, ref, goOffline } from "firebase/database";
 
 dotenv.config();
 const {FIREBASE_URL,LIST_URL,FIREBASE_DB} = process.env; // ENV
-const firebaseConfig = {
-    databaseURL: FIREBASE_URL,
-};
 (async () => {
     const app = initializeApp({databaseURL: FIREBASE_DB});
     const db = getDatabase(app);
     const nowRef = ref(db, `nowList`);
     const prevRef = ref(db, `prevList`);
-    console.log("v2");
+
+    console.log("step1");
+    await get(nowRef).then(async (snapshot) => {
+        if (snapshot.exists()) {
+            await set(prevRef,snapshot.val());
+            console.log(snapshot.val().length);
+        } else {
+            console.log("No data available");
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+    console.log("step2");
     await set(nowRef,null);
 
     console.log("step3");
