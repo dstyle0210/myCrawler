f4a9dc731327fa1df4abdcd720269f2b9c1a2f0c
dstyle0210 <dstyle0210@gmail.com>
2025-02-14 16:47:17 +0900

    3333

--- crawler/list.ts
+++ crawler/list.ts
@@ -4,19 +4,16 @@ import {initializeApp} from "firebase/app";
 import { getDatabase, set, get, ref, goOffline } from "firebase/database";
 
 dotenv.config();
-const {FIREBASE_URL,LIST_URL} = process.env; // ENV
-console.log(FIREBASE_URL);
-console.log(LIST_URL);
-
+const {FIREBASE_URL,LIST_URL,FIREBASE_DB} = process.env; // ENV
 const firebaseConfig = {
     databaseURL: FIREBASE_URL,
 };
 (async () => {
-    const app = initializeApp(firebaseConfig);
+    const app = initializeApp({databaseURL: FIREBASE_DB});
     const db = getDatabase(app);
     const nowRef = ref(db, `nowList`);
     const prevRef = ref(db, `prevList`);
-    console.log("v1");
+    console.log("v2");
     await set(nowRef,null);
 
     console.log("step3");
