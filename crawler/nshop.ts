1b600694e150f8ad946a801e65a20f26f90bb871
dstyle0210 <dstyle0210@gmail.com>
2026-07-08 09:59:51 +0900

    action 변경.

--- crawler/nshop.ts
+++ crawler/nshop.ts
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 import request from 'request';
 import TelegramBot from "node-telegram-bot-api";
 const { NAVER_CLIENT_ID, NAVER_CLIENT_SECRET } = process.env; // ENV
-const {TG_TOKEN_MABONGPAPA,TG_CHATID_MABONGPAPA} = process.env; // ENV
+const { TG_TOKEN_MABONGPAPA, TG_CHATID_MABONGPAPA } = process.env; // ENV
 if (!NAVER_CLIENT_ID || !NAVER_CLIENT_SECRET) {
     dotenv.config();
 };
-const bot = new TelegramBot(TG_TOKEN_MABONGPAPA, {polling: false});
-const items = ['아디다스 KS2229', '아디다스 KS2231','아디다스 JQ6637'];
+const bot = new TelegramBot(TG_TOKEN_MABONGPAPA, { polling: false });
+const items = ['아디다스 KS2229', '아디다스 KS2231', '아디다스 JQ6637'];
 items.forEach(item => {
     request.get({
         url: `https://openapi.naver.com/v1/search/shop.json?sort=asc&query=${encodeURI(item)}&exclude=used:rental:cshop`,
@@ -17,14 +17,14 @@ items.forEach(item => {
         let message = "";
         if (!error && response.statusCode == 200) {
             const data = JSON.parse(body);
-            if(data.items.length === 0){
+            if (data.items.length === 0) {
                 message = `[nShop] ${item} 검색 결과가 없습니다.`;
-            }else{
+            } else {
                 const { title, lprice, link } = data.items[0];
-                message = "[nShop]\n"+title.replace(/<[^>]*>?/g, '')+"\n"+lprice + "원\n"+link;
+                message = "[nShop]\n" + title.replace(/<[^>]*>?/g, '') + "\n" + lprice + "원\n" + link;
             };
         } else {
-            message = "[nShop] "+'error = ' + response.statusCode;
+            message = "[nShop] " + 'error = ' + response.statusCode;
         };
         bot.sendMessage(TG_CHATID_MABONGPAPA, message);
     });
